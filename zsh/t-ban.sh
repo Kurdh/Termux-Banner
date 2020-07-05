d43c3038800a007135de14e144ce19dcd5efc768
dana mantk <61474497+Kurdh@users.noreply.github.com>
2020-07-05 13:34:25 +0300

    Update t-ban.sh

--- zsh/t-ban.sh
+++ zsh/t-ban.sh
@@ -18,10 +18,10 @@ echo -e $yellow "\e[1m                         -Sutariya Parixit "
 echo " "
 echo " "
 echo " "
-echo -e "\e[1m\e[33m\nWhat is Your \e[31mBanner \e[33mName\e[32m :\n\n"
+echo -e "\e[1m\e[33m\n nawy bannerakat chy bet? \e[31m \e[33m\e[32m :\n\n"
 read varbanner
 echo
-echo -e "\e[1m\e[33m\nWhat is Your Cowsay Name\e[32m :\n\n "
+echo -e "\e[1m\e[33m\n nawy psht wenaka chy bet? \e[32m :\n\n "
 read varcowsay
 echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
 echo "toilet -f big '    $varbanner' -F gay | lolcat" > 84nn3r.txt
